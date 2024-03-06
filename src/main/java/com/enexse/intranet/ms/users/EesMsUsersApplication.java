9c3aebfe09e201384b95a87e4e28dde5c1d80c60
Nzeukang Nimpa Randrin <nzeukangrandrin@gmail.com>
2024-03-06 14:30:38 +0100

    Test Update

--- src/main/java/com/enexse/intranet/ms/users/EesMsUsersApplication.java
+++ src/main/java/com/enexse/intranet/ms/users/EesMsUsersApplication.java
@@ -1,5 +1,7 @@
 package com.enexse.intranet.ms.users;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -10,9 +12,14 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}/*scanBasePackages={"com.enexse.intranet.ms.users.repositories"}*/)
 @EnableEurekaClient
 @EnableFeignClients
+@Slf4j
 public class EesMsUsersApplication {
 
+    @Value("${eureka.client.serviceUrl.defaultZone}")
+    private static String defaultZone;
+
     public static void main(String[] args) {
+        log.info("Environment defaultZone: " + defaultZone);
         SpringApplication.run(EesMsUsersApplication.class, args);
     }
 
